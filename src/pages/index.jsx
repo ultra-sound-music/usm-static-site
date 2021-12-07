09810aa6afc126e0a6d112c0b1ea8cc36571efee
Gabriel <g@brielcastillo.me>
2021-12-06 16:39:22 -0800

    update tagline + header styling

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -42,7 +42,7 @@ class Index extends React.Component {
             </div>
             <div className={styles.header}>
               <h1 className={styles.title}>Ultra Sound Music</h1>
-              <h2 className={styles.subtitle}>Building the crossroads between music and NFTs. <br />Join our community to get notified on the launch.</h2>
+              <h2 className={styles.subtitle}>A collaborative NFT platform for virtual musicians</h2>
             </div>
 
             {this.renderSocialLinks()}
